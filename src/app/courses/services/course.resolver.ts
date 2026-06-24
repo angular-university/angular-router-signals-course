29c7c4e078d352e847a49d72d303d55131a2abc1
vasco <jhades.dev@gmail.com>
2026-06-24 18:09:50 +0200

    Angular Router In Depth (Signals Edition) course

--- src/app/courses/services/course.resolver.ts
+++ src/app/courses/services/course.resolver.ts
@@ -1,9 +1,15 @@
 import {inject} from '@angular/core';
-import {ResolveFn} from '@angular/router';
+import {ResolveFn, Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
+import {firstValueFrom} from 'rxjs';
 import {Course} from '../model/course';
 
-export const courseResolver: ResolveFn<Course> = (route) => {
+export const courseResolver: ResolveFn<Course> = async (route) => {
+    const router = inject(Router);
+    const course = router.currentNavigation()?.extras?.state?.['course'] as Course;
+    if (course) return course;
+
     const courseUrl = route.paramMap.get('courseUrl');
-    return inject(HttpClient).get<Course>(`/api/courses/${courseUrl}`);
+    const http = inject(HttpClient);
+    return firstValueFrom(http.get<Course>(`/api/courses/${courseUrl}`));
 };
