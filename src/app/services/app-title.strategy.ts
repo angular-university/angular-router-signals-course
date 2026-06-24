44c79c15e8f9f3163faef7f99727c60f9de8a7fc
vasco <jhades.dev@gmail.com>
2026-06-24 15:47:22 +0200

    Angular Router In Depth (Signals Edition) course

--- src/app/services/app-title.strategy.ts
+++ src/app/services/app-title.strategy.ts
@@ -1,12 +1,15 @@
-import {Service} from '@angular/core';
+import {inject, Service} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 import {RouterStateSnapshot, TitleStrategy} from '@angular/router';
 
 @Service()
 export class AppTitleStrategy extends TitleStrategy {
+    private readonly title = inject(Title);
+
     override updateTitle(snapshot: RouterStateSnapshot) {
         const title = this.buildTitle(snapshot);
-        document.title = title
-            ? `${title} — Angular Router In Depth`
-            : 'Angular Router In Depth';
+        this.title.setTitle(
+            title ? `${title} — Angular Router In Depth` : 'Angular Router In Depth'
+        );
     }
 }
