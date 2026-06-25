2186c06638752673b1ac398fbb138082cfe6ad65
vasco <jhades.dev@gmail.com>
2026-06-25 14:34:32 +0200

    Angular Router In Depth (Signals Edition) course

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import {ApplicationConfig, provideZonelessChangeDetection} from '@angular/core';
+import {ApplicationConfig, inject, provideZonelessChangeDetection} from '@angular/core';
 import {
+    NavigationError,
     provideRouter,
+    RedirectCommand,
+    Router,
     withComponentInputBinding,
     withInMemoryScrolling,
+    withNavigationErrorHandler,
     withPreloading,
     withRouterConfig,
     withViewTransitions,
@@ -21,6 +25,11 @@ export const appConfig: ApplicationConfig = {
             withRouterConfig({paramsInheritanceStrategy: 'always'}),
             withInMemoryScrolling({scrollPositionRestoration: 'enabled'}),
             withViewTransitions(),
+            withNavigationErrorHandler((error: NavigationError) => {
+                console.error('Navigation error:', error);
+                const router = inject(Router);
+                return new RedirectCommand(router.parseUrl('/page-not-found'));
+            }),
         ),
         provideHttpClient(withInterceptorsFromDi()),
     ]
