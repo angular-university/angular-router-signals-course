9c998879c0c2309b08cfaacf7b398fc58d02f1e8
vasco <jhades.dev@gmail.com>
2026-07-15 17:17:36 +0200

    Angular Router In Depth (Signals Edition) course

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, input} from '@angular/core';
+import {Component, effect, inject, input} from '@angular/core';
 import {toSignal} from '@angular/core/rxjs-interop';
 import {filter} from 'rxjs';
 import {LoadingService} from './loading.service';
@@ -19,8 +19,40 @@ import {
     imports: [],
 })
 export class LoadingComponent {
+
     private loadingService = inject(LoadingService);
 
     readonly detectRoutingOngoing = input(false);
     readonly loading = this.loadingService.loading;
+
+    readonly router = inject(Router);
+
+  private readonly navStart = toSignal(
+    this.router.events.pipe(
+      filter(e => [NavigationStart, RouteConfigLoadStart].some(T => e instanceof T))
+    )
+  );
+
+  private readonly navEnd = toSignal(
+    this.router.events.pipe(
+      filter(e => [NavigationEnd, NavigationError, NavigationCancel, RouteConfigLoadEnd].some(T => e instanceof T))
+    )
+  );
+
+  constructor() {
+    effect(() => {
+      const start = this.navStart();
+      const detect = this.detectRoutingOngoing();
+      if (!start || !detect) return;
+      this.loadingService.loadingOn();
+    });
+
+    effect(() => {
+      const end = this.navEnd();
+      const detect = this.detectRoutingOngoing();
+      if (!end || !detect) return;
+      this.loadingService.loadingOff();
+    });
+  }
+
 }
