2731cd1cc93329c9b575780e483f9374ddde6027
vasco <jhades.dev@gmail.com>
2026-07-23 16:48:56 +0200

    Angular Router In Depth (Signals Edition) course

--- src/app/courses/services/course.resolver.ts
+++ src/app/courses/services/course.resolver.ts
@@ -5,7 +5,14 @@ import {firstValueFrom} from 'rxjs';
 import {Course} from '../model/course';
 
 export const courseResolver: ResolveFn<Course> = async (route) => {
+  const router = inject(Router);
+  const course = router.currentNavigation()?.extras?.info?.['course'] as Course;
+  if (course) {
+    console.log('[courseResolver] Course loaded from navigation extras info:', course);
+    return course;
+  }
   const courseUrl = route.paramMap.get('courseUrl');
+  console.log('No course in navigation extras state, fetching from API for courseUrl:', courseUrl);
   const http = inject(HttpClient);
   return firstValueFrom(http.get<Course>(`/api/courses/${courseUrl}`));
 };
