d78e575d8746509b701b586178b59060ea569d75
vasco <jhades.dev@gmail.com>
2026-07-24 14:55:54 +0200

    Angular Router In Depth (Signals Edition) course

--- src/app/courses/services/course.resolver.ts
+++ src/app/courses/services/course.resolver.ts
@@ -1,12 +1,12 @@
 import {inject} from '@angular/core';
-import {ResolveFn, Router} from '@angular/router';
+import {RedirectCommand, ResolveFn, Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {firstValueFrom} from 'rxjs';
 import {Course} from '../model/course';
 
-export const courseResolver: ResolveFn<Course> = async (route) => {
+export const courseResolver: ResolveFn<Course> = async (route, state) => {
   const router = inject(Router);
-  const course = router.currentNavigation()?.extras?.info?.['course'] as Course;
+  let course = router.currentNavigation()?.extras?.info?.['course'] as Course;
   if (course) {
     console.log('[courseResolver] Course loaded from navigation extras info:', course);
     return course;
@@ -14,5 +14,11 @@ export const courseResolver: ResolveFn<Course> = async (route) => {
   const courseUrl = route.paramMap.get('courseUrl');
   console.log('No course in navigation extras state, fetching from API for courseUrl:', courseUrl);
   const http = inject(HttpClient);
-  return firstValueFrom(http.get<Course>(`/api/courses/${courseUrl}`));
+  course = await firstValueFrom(http.get<Course>(`/api/courses/${courseUrl}`));
+  if (course) {
+    return course;
+  }
+  return new RedirectCommand(router.parseUrl("not-found"), {
+    browserUrl: state.url
+  });
 };
